c2dc8f61029ab46de5e8a97d82766a62aaef918b
Dáda <marvdav21v@gyri.cz>
2025-10-09 19:03:23 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,68 +7,44 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
 
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [data, setData] = useState<any>({});
-  const router = useRouter();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [data, setData] = useState<any>({});
+    const router = useRouter();
 
-  if (loggedIn) {
-    if (data.profile === 3) {
-      return (
-        <div className={styles.container}>
-          <aside className={styles.side}>
-            <button className={styles.btn} onClick={() => router.push("/onas")}>O NÁS</button>
-            <button className={styles.btn} onClick={() => router.push("/teorie")}>TEORIE</button>
-            <button className={styles.btn} onClick={() => router.push("/priklady")}>PŘÍKLADY</button>
-          </aside>
-          <main className={styles.main}>
-            <h1 className={styles.title}>ňáký yappování pro gulyho a vojtu o tom jak jsme tohle udělali *žákovský účet*</h1>
-            <p>{JSON.stringify(data)}</p>
-          </main>
-        </div>
-      );
-    } else if (data.profile === 2) {
-      return (
-        <div className={styles.container}>
-          <aside className={styles.side}>
-            <button className={styles.btn} onClick={() => router.push("/onas")}>O NÁS</button>
-            <button className={styles.btn} onClick={() => router.push("/teorie")}>TEORIE</button>
-            <button className={styles.btn} onClick={() => router.push("/priklady")}>PŘÍKLADY</button>
-          </aside>
-          <main className={styles.main}>
-            <h1 className={styles.title}>ňáký yappování pro gulyho a vojtu o tom jak jsme tohle udělali *učitelský účet*</h1>
-            <p>{JSON.stringify(data)}</p>
-          </main>
-        </div>
-      );
-    } else if (data.profile === 1) {
-      return (
-        <div className={styles.container}>
-          <aside className={styles.side}>
-            <button className={styles.btn} onClick={() => router.push("/onas")}>O NÁS</button>
-            <button className={styles.btn} onClick={() => router.push("/teorie")}>TEORIE</button>
-            <button className={styles.btn} onClick={() => router.push("/priklady")}>PŘÍKLADY</button>
-          </aside>
-          <main className={styles.main}>
-            <h1 className={styles.title}>ňáký yappování pro gulyho a vojtu o tom jak jsme tohle udělali *adminův účet*</h1>
-            {/*<p>{JSON.stringify(data)}</p>*/}
-          </main>
-        </div>
-      );
+    if (loggedIn) {
+        if (data.profile === 3) {
+            return (
+                <>
+                    {router.push("/onas")}
+                </>
+            );
+        } else if (data.profile === 2) {
+            return (
+                <>
+                    {router.push("/onas")}
+                </>
+            );
+        } else if (data.profile === 1) {
+            return (
+                <>
+                    {router.push("/onas")}
+                </>
+            );
+        }
+    } else {
+        return (
+            <div className={styles.kontejnr}>
+                <main className={styles.lockin}>
+                    <h1 className={styles.plz}>PROSÍM, PŘIHLAŠTE SE</h1>
+                    <div className={styles.inputRow}>
+                        <input type="text" placeholder="Uživatelské jméno" className={styles.input} onChange={(e) => setUsername(e.target.value)} />
+                        <input type="password" placeholder="Heslo" className={styles.input} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button className={styles.btn} onClick={() => backendCalls.login(username, password, setLoggedIn, setData)}>PŘIHLÁSIT SE</button>
+                </main >
+            </div >
+        );
     }
-  } else {
-    return (
-      <div className={styles.kontejnr}>
-        <main className={styles.lockin}>
-          <h1 className={styles.plz}>PROSÍM, PŘIHLAŠTE SE</h1>
-          <div className={styles.inputRow}>
-            <input type="text" placeholder="Uživatelské jméno" className={styles.input} onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder="Heslo" className={styles.input} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-            <button className={styles.btn} onClick={() => backendCalls.login(username, password, setLoggedIn, setData)}>PŘIHLÁSIT SE</button>
-        </main >
-      </div >
-    );
-  }
 }
