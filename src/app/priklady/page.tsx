8bd130d415f837d081818621120cfc909607277e
(OT1989) <(turaond21v@gyri.cz)>
2025-10-20 19:38:49 +0200

    Promazání zbytečných poznámek v priklady/page.tsx a teorie/page.tsx

--- src/app/priklady/page.tsx
+++ src/app/priklady/page.tsx
@@ -175,37 +175,3 @@ export default function Priklady() {
     </div>
   );
 }
-
-
-
-
-
-
-/* starý model příkladů
-
-'use client';
-
-import styles from '../Home.module.css';
-import React, { useState } from 'react';
-import * as backendCalls from '../backendCalls';
-import { useRouter } from 'next/navigation';
-
-export default function Home() {
-  const router = useRouter();
-  const [data, setData] = useState<any>({});
-
-  return (
-    <div className={styles.container}>
-      <aside className={styles.side}>
-      <button className={styles.btn} onClick={() => router.push("/onas")}>O NÁS</button>
-      <button className={styles.btn} onClick={() => router.push("/teorie")}>TEORIE</button>
-      <button className={styles.btn} onClick={() => router.push("/priklady")}>PŘÍKLADY</button>
-    </aside>
-    <main className={styles.main}>
-      <h1 className={styles.title}>1 + 1 = X *adminův účet*</h1>
-      {/*<p>{JSON.stringify(data)}</p>*//*}
-    </main>
-  </div>
-  );
-}
-  */
