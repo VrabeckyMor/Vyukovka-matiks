76771100b172d94c608944614539ef31afe046ea
(OT1989) <(turaond21v@gyri.cz)>
2025-11-24 19:20:41 +0100

    Přidal jsem 5 typů příkladů a globání skóre, které zůstává stejné ve všech tématech. Pro funkčnost je potřeba dodělat backend a lištu s výběrem typů příkladů.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getSessionCookie } from 'better-auth/cookies'
 
 export async function middleware(request: NextRequest) {
-  const session = getSessionCookie(request)
-  console.log('Middleware Session:', session);
+  const session = await getSessionCookie(request)
+  console.log('Middleware Session:', session)
 
-  const isProtected = !request.nextUrl.pathname.startsWith('/login')
-
-  if (isProtected && !session) {
-    return NextResponse.redirect(new URL('/login', request.url))
+  if (!session) {
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
   return NextResponse.next()
